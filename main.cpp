50e967d50e496d7f414fb59961708fba8806a083
3ab2wy1911 <3ab2wy1911@gmail.com>
2023-04-24 18:09:24 +0200

    Longest Valid Parentheses problem

--- main.cpp
+++ main.cpp
@@ -19,9 +19,21 @@ void function2()
 
 }
 //________________________________________________________________
-void function4()
+void  Longest_Valid_Parentheses(string & str)
 {
-
+    Stack<char> s;
+    auto n = str.length();
+    for(int i=0;i<n;i++) {
+        if(s.isEmpty() or s.Top() == str[i] or (s.Top() == ')' and str[i] == '('))
+        {
+            s.push(str[i]);
+        }
+        else
+        {
+            s.pop();
+        }
+    }
+    cout << n-s.stackSize() << endl;
 }
 //________________________________________________________________
 void function5()
@@ -49,5 +61,17 @@ int main() {
 //test cases of function 1
 //test cases of function 2
 //...
+//________________________________________________________________
+//Test cases of Longest Valid Parentheses problem
+    cout<<"Test cases of Longest Valid Parentheses problem:\n";
+    string str1 = "(()";
+    string str2 = "";
+    string str3 = ")()()))";
+    cout<<"input : "<< str1<<"\noutput:";
+    Longest_Valid_Parentheses(str1);
+    cout<<"input : "<< str2<<"\noutput:";
+    Longest_Valid_Parentheses(str2);
+    cout<<"input : "<< str3<<"\noutput:";
+    Longest_Valid_Parentheses(str3);
 //________________________________________________________________
 }
